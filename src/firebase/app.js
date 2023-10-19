3c68602cfff52a253fc5d83c9aa37c8159e31331
ivan di gruttola <ivansandigruttola@gmail.com>
2023-10-19 13:33:15 -0300

    firebase

--- src/firebase/app.js
+++ src/firebase/app.js
@@ -2,8 +2,15 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth"
 
-//file config
-import { firebaseConfig } from './config.js'
+const firebaseConfig = {
+  apiKey: "AIzaSyCgsle8cpTvEDeao7djLLJ32NoUKTfPYbI",
+  authDomain: "digrutt-deposito.firebaseapp.com",
+  projectId: "digrutt-deposito",
+  storageBucket: "digrutt-deposito.appspot.com",
+  messagingSenderId: "1085167861052",
+  appId: "1:1085167861052:web:507b7b2ccdc6aed91bb239",
+  measurementId: "G-SG3SPJ1T15"
+};
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
